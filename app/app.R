5cdcd7e7675d43263ae807478fa6c7c65f479882
brndngrhm <brendanvgraham@gmail.com>
2016-01-05 13:33:57 -0500

    changes app back to basic one

--- app/app.R
+++ app/app.R
@@ -7,59 +7,38 @@ library(shinyapps)
 #install.packages("devtools")
 #install_github( repo = "shinyapps", username="rstudio" )
 
-source("nfl_team_stats.R")
-
-#UI----
 
 # begin each app with template:
 #ui <- fluidPage()
 #server <- function(input, output){}
 #shinyApp(ui = ui, server = server)
 
+
+#UI----
 # add elements as arguments to fluidPage()
 # create reactive inputs with an Input() function
 # display reactive results with an Output() function
 
 
 ui <- fluidPage(
-  # Give the page a title
-  titlePanel("NFL Team Stats"),
-  
-  # Generate a row with a sidebar
-  sidebarLayout(      
-    
-    # Define the sidebar with one input
-    sidebarPanel(
-      selectInput("team", "Team:", 
-                  choices=levels(stats$team))
-    ),
-    
-    # Create a spot for the barplot
-    mainPanel(
-      plotOutput("downs.plot")  
-    )
-    
-  )
+  sliderInput(inputId = "num", 
+              label = "Choose a Number", 
+              value=25, min=1, max=100),
+  plotOutput("hist")
 )
 
 
 #Server----
 #server function assembles inputs into outputs
-
 #3 rules:
 # save output you build to output$
 # build output objects with render function
 # access  inout values with input$ to build rendered outputs
 
 server <- function(input, output){
-  output$downs.plot <- renderPlot(
-    
-    # Render a barplot
-    barplot(x = stats$year, y= stats$firstdowns.game[input == "2015"],
-            main="First Downs",
-            ylab="Number of Telephones",
-            xlab="Year")
-  )
+  output$hist <- renderPlot({
+    hist(rnorm(input$num))
+  })
 }
 
 
